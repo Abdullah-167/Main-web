21f0bb79af0ec7cb2a6fc6f28b80255c9729630d
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-05-29 22:20:03 +0500

    commit done

--- src/Components/MiscroSoft/Vedio.tsx
+++ src/Components/MiscroSoft/Vedio.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 import Button from '../Button'
 import useTranslation from '@/hooks/useTranslation';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 
 const Vedio = () => {
 
@@ -20,8 +21,8 @@ const Vedio = () => {
         <div className='vedio-bg'>
             <div className='container relative'>
                 <div className='flex flex-col lg:flex-row gap-16 sm:gap-24 py-20 sm:py-32'>
-                    <div className='w-full flex justify-between items-center'>
-                        <iframe title='rafiky' className='w-full h-[300px] sm:h-[600px]' loading="lazy" src="https://www.youtube.com/embed/K1xWE4MdmTY?feature=oembed"></iframe>
+                    <div className='w-full flex justify-between items-center h-[300px] sm:h-[600px]'>
+                        <LiteYouTubeEmbed title='rafiky' id="K1xWE4MdmTY" />
                     </div>
                     <div>
                         <div className='mb-10'>
