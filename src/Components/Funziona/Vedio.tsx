f2371bce538ffea1e4889af11b99e292165d2f6b
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-05-29 22:39:28 +0500

    commit

--- src/Components/Funziona/Vedio.tsx
+++ src/Components/Funziona/Vedio.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Button from "../Button";
 import BgImage from "../../../public/pink.png";
 import useTranslation from "@/hooks/useTranslation";
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 
 const Vedio = () => {
   const { t } = useTranslation();
@@ -16,15 +17,15 @@ const Vedio = () => {
         <div className="xl:flex  gap-24 py-20 sm:py-32 pb-[100px]">
           <div>
             <div className="flex justify-center items-center mb-6">
-             
-                <div>
-                  <h3 className="text-[#6A1ACC] text-3xl sm:text-[45px] font-bold pt-10 leading-[55px]">
-                   
-                  </h3>
-                  <p className="text-base font-normal text-black pbF-4 max-w-[750px] pb-3 leading-7">
-                  {t("videoInterpreted_para")}  
-                  </p>
-              
+
+              <div>
+                <h3 className="text-[#6A1ACC] text-3xl sm:text-[45px] font-bold pt-10 leading-[55px]">
+
+                </h3>
+                <p className="text-base font-normal text-black pbF-4 max-w-[750px] pb-3 leading-7">
+                  {t("videoInterpreted_para")}
+                </p>
+
               </div>
             </div>
             <div className="mb-10">
@@ -36,11 +37,10 @@ const Vedio = () => {
               />
             </div>
           </div>
-          <div className="flex justify-center sm:pt-20 self-center">
-            <iframe
-              className="w-[400px] sm:w-[550px] md:w-[700px] xl:w-[700px] h-[300px] sm:h-[400px] mx-auto"
-              src="https://www.youtube.com/embed/K1xWE4MdmTY?feature=oembed" loading="lazy"
-            ></iframe>
+          <div className="flex justify-center sm:pt-20 self-center w-[400px] sm:w-[550px] md:w-[700px] xl:w-[700px] h-[300px] sm:h-[400px] mx-auto">
+            <LiteYouTubeEmbed
+              id="K1xWE4MdmTY" title="rafiky"
+            />
           </div>
         </div>
       </div>
